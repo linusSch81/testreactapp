6abdd54371cdc854b8d96a71393ba8c63b13a896
81linsch <81linsch@gafe.molndal.se>
2021-01-21 00:54:56 +0100

    Updated SignInView with onClick and onChage @setLoggedInUser and @setPassword - url: https://www.youtube.com/watch?v=-WTe7xhPJNk&list=PLo-BJUIALMg---6zMWhJE7iaJ4YBSRkWb&index=11

--- src/view/SignInView.js
+++ src/view/SignInView.js
@@ -5,13 +5,41 @@ export const SignInView = () => {
   // const [loggedInUser, setLoggedInUser] = useState(123);
   // const [loggedInUser, setLoggedInUser] = useState(["array1","array2","array3"]);
   // const [loggedInUser, setLoggedInUser] = useState({name: "Joe"});
-  
+  const [password, setPassword] = useState();
+
   // return <h1>This is the Sign In View</h1>;
-  return(
+  return (
     <>
-    <h1>{loggedInUser}</h1>
-    <button onClick={() => setLoggedInUser("You have clicked the button")}>Update the state value</button>  
+      <h1>Hello {loggedInUser}</h1>
+      <p>
+        <button onClick={() => setLoggedInUser("You have clicked the button")}>
+          Update the state value
+        </button>
+      </p>
+     
+      <p>
+        <input
+          placeholder="Update the state value"
+          onChange={(event) => setLoggedInUser(event.target.value)}
+        />
+      </p>
+      <hr />
+      <p>
+        <span>Username: </span>
+        <input
+          placeholder="Username"
+          onChange={(event) => setLoggedInUser(event.target.value)}
+        />
+      </p>
+      <p>
+        <span>Password: </span>
+        <input
+          placeholder="******"
+          onChange={(event) => setPassword(event.target.value)}
+          type="password"
+        />
+      </p>
+      <p><button>Login</button></p>
     </>
-    
-  )
+  );
 };
