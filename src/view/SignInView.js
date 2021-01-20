d13fd8580b8a4a4889681c51a6d0cbf64f1cec25
81linsch <81linsch@gafe.molndal.se>
2021-01-21 00:34:47 +0100

    Updated SignInView with updateState

--- src/view/SignInView.js
+++ src/view/SignInView.js
@@ -1,4 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
 export const SignInView = () => {
-  return (<h1>This is the Sign In View</h1>);
+  const [loggedInUser, setLoggedInUser] = useState("Hello user");
+  // const [loggedInUser, setLoggedInUser] = useState(123);
+  // const [loggedInUser, setLoggedInUser] = useState(["array1","array2","array3"]);
+  // const [loggedInUser, setLoggedInUser] = useState({name: "Joe"});
+  
+  // return <h1>This is the Sign In View</h1>;
+  return(
+    <>
+    <h1>{loggedInUser}</h1>
+    <button onClick={() => setLoggedInUser("You have clicked the button")}>Update the state value</button>  
+    </>
+    
+  )
 };
